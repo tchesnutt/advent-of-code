a0c7f86020e02ff7a76ce47b015e26668569928e
Taylor Chesnutt <taylor.chesnutt@gmail.com>
2024-12-01 11:51:24 -0800

    rm target

--- day-1/target/debug/day-1.d
+++
@@ -1,1 +0,0 @@
-/home/taylorchesnutt/workspace/advent-of-code-2024/day-1/target/debug/day-1: /home/taylorchesnutt/workspace/advent-of-code-2024/day-1/src/main.rs
--- day-1/target/debug/deps/day_1-1c6c0d7e85bb4e81.d
+++
@@ -1,5 +0,0 @@
-/home/taylorchesnutt/workspace/advent-of-code-2024/day-1/target/debug/deps/libday_1-1c6c0d7e85bb4e81.rmeta: src/main.rs
-
-/home/taylorchesnutt/workspace/advent-of-code-2024/day-1/target/debug/deps/day_1-1c6c0d7e85bb4e81.d: src/main.rs
-
-src/main.rs:
