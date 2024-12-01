d36a195c3f471de83451855a7f70af94b3ae9c4f
Taylor Chesnutt <taylor.chesnutt@gmail.com>
2024-12-01 12:23:34 -0800

    day-1 part 2 :)

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,14 +1,50 @@
 use std::fs::read_to_string;
 use std::collections::BinaryHeap;
 use std::cmp::Reverse;
+use std::collections::HashMap;
 
 
 fn main() {
     let lines = read_lines("./input.txt");
 
+    
+    calc_distance_between(lines.clone());
+    calc_similarity_score(lines.clone());
+}
+
+fn calc_similarity_score(lines: Vec<String>) {
+    let mut count_map: HashMap<i32, i32> = HashMap::new();
+    let mut left_list: Vec<i32> = Vec::new();
+
+    for line in lines {
+        let line: Vec<&str> = line.split("   ").collect();
+        for (index, num) in line.iter().enumerate() {
+            let num: i32 = num.parse().unwrap();
+            match index {
+                0 => left_list.push(num),
+                1 => *count_map.entry(num).or_insert(0) += 1,
+                _ => println!("ANOTHER THING WRONG MY DUDE")
+            }
+        }
+    }
+
+    let mut total_similarity = 0;
+    for num in left_list {
+        let count = count_map.get(&num).copied().unwrap_or(0);
+        let similarity = num * count;
+        println!("num: {}, count: {}, similarty: {}, total_similarty: {}", num, count, similarity, total_similarity);
+        total_similarity += similarity
+    }
+
+    println!("Totat Similarity: {}", total_similarity);
+
+    
+}
+
+fn calc_distance_between(lines: Vec<String>) {
     let mut left_heap = BinaryHeap::new();
     let mut right_heap = BinaryHeap::new();
-
+    
     for line in lines {
         let nums: Vec<&str> = line.split("   ").collect();
         for (index, num) in nums.iter().enumerate() {
@@ -20,15 +56,6 @@ fn main() {
             }
         }
     }
-
-    calc_distance_between(left_heap.clone(), right_heap.clone());
-}
-
-fn calc_similarity_score(mut left_heap: BinaryHeap<Reverse<i32>>, mut right_heap: BinaryHeap<Reverse<i32>>) {
-
-}
-
-fn calc_distance_between(mut left_heap: BinaryHeap<Reverse<i32>>, mut right_heap: BinaryHeap<Reverse<i32>>) {
     let mut total_distance = 0;
     while !left_heap.is_empty() && !right_heap.is_empty() {
         let left_num = left_heap.pop();
@@ -40,7 +67,7 @@ fn calc_distance_between(mut left_heap: BinaryHeap<Reverse<i32>>, mut right_heap
         total_distance += distance;
     }
     
-    println!("{}", total_distance);
+    println!("Total Distance: {}", total_distance);
 }
 
 fn read_lines(filename: &str) -> Vec<String> {
